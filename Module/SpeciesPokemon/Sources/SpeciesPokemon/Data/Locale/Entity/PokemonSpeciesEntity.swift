5395586a2a89e8e675c31632825cccaab7dbed74
rivaldofez <rivaldofez@gmail.com>
2024-04-01 23:03:01 +0700

    implement pokemon species entity model

--- Module/SpeciesPokemon/Sources/SpeciesPokemon/Data/Locale/Entity/PokemonSpeciesEntity.swift
+++ Module/SpeciesPokemon/Sources/SpeciesPokemon/Data/Locale/Entity/PokemonSpeciesEntity.swift
@@ -5,4 +5,22 @@
 //  Created by rivaldo on 01/04/24.
 //
 
-import Foundation
+import RealmSwift
+
+public class PokemonSpeciesEntity: Object {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var baseHappines: Int
+    @Persisted var captureRate: Int
+    @Persisted var color: String
+    @Persisted var about: String
+    @Persisted var genderRate: String
+    @Persisted var genus: String
+    @Persisted var growthRate: String
+    @Persisted var habitat: String
+    @Persisted var hatchCounter: Int
+    @Persisted var isLegendary: Bool
+    @Persisted var isMythical: Bool
+    @Persisted var isBaby: Bool
+    @Persisted var shape: String
+    @Persisted var eggGroups: String
+}
