62a2a60d25e3325f2b03d6874bf8f621afd4490f
rivaldofez <rivaldofez@gmail.com>
2024-04-02 00:25:06 +0700

    create aboutcellmodel for mapping ui element

---
+++ PokedexApp/Pokedex/Core/Domain/Model/AboutCellModel.swift
@@ -0,0 +1,20 @@
+//
+//  AboutCellModel.swift
+//  pokedex
+//
+//  Created by rivaldo on 02/04/24.
+//
+
+import Foundation
+
+import Foundation
+
+struct AboutCellModel {
+    let name: String
+    let item: [ItemCellModel]
+}
+
+struct ItemCellModel {
+    let title: String
+    let value: String
+}
