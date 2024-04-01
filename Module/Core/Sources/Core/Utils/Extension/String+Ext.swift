8ae9d4c43fe023d9fb4c9ac6d0a729f6056a4217
rivaldofez <rivaldofez@gmail.com>
2024-04-01 20:32:28 +0700

    add extension to clean and decode string description pokemon

---
+++ Module/Core/Sources/Core/Utils/Extension/String+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by rivaldo on 01/04/24.
+//
+
+import Foundation
+
+extension String {
+    public func utf8EncodedString() -> String {
+        let messageData = self.data(using: .nonLossyASCII)
+        let text = String(data: messageData!, encoding: .utf8) ?? ""
+        return text
+    }
+    
+    public func utf8DecodedString() -> String {
+        let data = self.data(using: .utf8)
+        let message = String(data: data!, encoding: .nonLossyASCII) ?? ""
+        return message
+    }
+}
