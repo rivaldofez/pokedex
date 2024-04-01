f17244ee9d22d5b0a814f4efdae6d6affd169ae1
rivaldofez <rivaldofez@gmail.com>
2024-04-01 21:10:53 +0700

    register custom fonts in package desc

--- Module/Common/Package.swift
+++ Module/Common/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Common"),
+            name: "Common",
+            resources: [
+                .process("Font/Comfortaa"),
+                .process("Font/Poppins")
+            ]
+        ),
         .testTarget(
             name: "CommonTests",
             dependencies: ["Common"]),
