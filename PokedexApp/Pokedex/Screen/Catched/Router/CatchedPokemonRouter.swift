3d0fee32fe04257ee9b6c2dd77ea5807380b5ab4
rivaldofez <rivaldofez@gmail.com>
2024-04-02 18:10:51 +0700

    implement catched pokemon router

---
+++ PokedexApp/Pokedex/Screen/Catched/Router/CatchedPokemonRouter.swift
@@ -0,0 +1,49 @@
+//
+//  CatchedPokemonRouter.swift
+//  pokedex
+//
+//  Created by rivaldo on 02/04/24.
+//
+
+import GeneralPokemon
+import CatchPokemon
+import Foundation
+
+protocol CatchedPokemonRouterProtocol {
+    var entry: CatchedPokemonViewController? { get set }
+    
+    static func createCatched() -> CatchedPokemonRouterProtocol
+    
+    func gotoDetailPokemon(with pokemon: PokemonDomainModel )
+}
+
+class CatchedPokemonRouter: CatchedPokemonRouterProtocol {
+    var entry: CatchedPokemonViewController?
+    
+    static func createCatched() -> CatchedPokemonRouterProtocol {
+        let router = CatchedPokemonRouter()
+    
+        var view: CatchedPokemonViewProtocol = CatchedPokemonViewController()
+        var presenter: CatchedPokemonPresenterProtocol = CatchedPokemonPresenter()
+        
+        view.presenter = presenter
+        presenter.router = router
+        presenter.view = view
+        presenter.getCatchPokemonInteractor = Injection().provideGetCatchPokemon()
+        presenter.putCatchPokemonInteractor = Injection().providePutCatchPokemon()
+        
+        router.entry = view as? CatchedPokemonViewController
+        
+        return router
+    }
+    
+    func gotoDetailPokemon(with pokemon: PokemonDomainModel) {
+        let detailPokemonRouter = DetailPokemonRouter.createDetailPokemon(with: pokemon)
+        guard let detailPokemonView = detailPokemonRouter.entry else { return }
+        guard let viewController = self.entry else { return }
+        
+        viewController.hidesBottomBarWhenPushed = true
+        viewController.navigationController?.pushViewController(detailPokemonView, animated: true)
+        viewController.hidesBottomBarWhenPushed = false
+    }
+}
