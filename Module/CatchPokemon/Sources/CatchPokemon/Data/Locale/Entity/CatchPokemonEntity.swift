64afd71ae987f0e2e519bccefc891f27a218f26e
rivaldofez <rivaldofez@gmail.com>
2024-04-02 12:10:24 +0700

    create catch pokemon entity

---
+++ Module/CatchPokemon/Sources/CatchPokemon/Data/Locale/Entity/CatchPokemonEntity.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by rivaldo on 02/04/24.
+//
+
+import RealmSwift
+import Foundation
+
+public class CatchPokemonEntity: Object {
+    @Persisted(primaryKey: true) public var id: Int
+    @Persisted public var name: String
+    @Persisted public var nickname: String
+    @Persisted public var image: String
+    @Persisted public var type: String
+    @Persisted public var addedDate: Date
+}
